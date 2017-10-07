41f33f4b5308a8fd2e8c7e6bcf118281bd98dd0b
Filipe Rodrigues <rodrigues.filipe@live.com.pt>
2017-10-07 04:15:27 +0100

    + includes

---
+++ includes/site.config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config['base_url'] = 'http://www.rodriguesfilipe.net/bitcoin'; // The url where the script is installed
+
+$config['site_title'] = 'Simple Bitcoin Value Calculator'; // Your websites name/title
+
+$config['site_desc'] = 'Bitcoin calculator for 28 currencies!'; // Your websites description
+
+$config['site_color'] = '#FF9900'; // This is the websites main color scheme
+
+$config['default_currency'] = 'EUR'; // The default currency code for the index page
+
+$config['cache_time'] = '60'; // Data cache time in seconds
+
+?>
---
+++ includes/wrapper/footer.template.php
@@ -0,0 +1,11 @@
+<div id="footer">
+	<div class="container">
+		<span class="pull-left">&COPY; <?php echo $config['site_title']; ?> 2017.</span>
+	</div>
+</div>     
+
+ <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script src="<?php echo $config['base_url']; ?>/assets/js/pace.min.js"></script>
+      <script src="<?php echo $config['base_url']; ?>/assets/js/bootstrap.min.js"></script>
+	</body>
+</html>
